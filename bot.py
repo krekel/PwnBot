a00ceeab4505edee8368d879bd41687c9dbb3d33
krekel <luisg.diazpr@gmail.com>
2018-04-15 20:35:08 -0400

    Reformatted code and implemented logger

--- bot.py
+++ bot.py
@@ -1,14 +1,36 @@
 import config
+import discord
+import logging
+
 from discord.ext import commands
 
+logging.basicConfig(filename='server.log',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-class PwnBot(commands.Bot):
-    def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or('!'), pm_help=True)
-        self.token = config.TOKEN
+logger = logging.getLogger(__name__)
 
-    async def on_ready(self):
-        print('Bot ready')
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), pm_help=True)
 
-bot = PwnBot()
-bot.run(bot.token)
+
+@bot.event
+async def on_ready():
+    print('Bot ready')
+
+
+@bot.event
+async def on_message(message):
+    # Bot messages will not be logged
+    if message.author.bot:
+        pass
+    else:
+        logger.info(f'AUTHOR: {message.author}, CHANNEL: {message.channel}, CONTENT: {message.content}, TYPE: {message.channel.type}')
+        await bot.process_commands(message)
+
+
+@bot.command()
+async def ping():
+    await bot.say('Pong')
+
+
+bot.run(config.TOKEN)
