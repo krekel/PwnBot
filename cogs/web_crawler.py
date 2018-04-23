403fba24c6210c90b1e12befcc7e580f761bc084
krekel <luisg.diazpr@gmail.com>
2018-04-22 22:17:24 -0400

    upcoming ctfs display is formatted

--- cogs/web_crawler.py
+++ cogs/web_crawler.py
@@ -23,13 +23,17 @@ class WebCrawler:
         if option not in arguments:
             await ctx.send('Invalid argument. Choose one of the following -> week, month or year')
 
+        embed = discord.Embed(colour=0x3DF270, title=f'Upcoming CTFs for this {option}')
         start, end = self._calculate_timestamp(option)
         print(start, end)
 
         async with self.bot.session.get(f'https://ctftime.org/api/v1/events/?limit=100&start={start}&finish={end}') as response:
                 upcoming = await response.json()
-                print(upcoming[0])
-        await ctx.send(upcoming[0]['title'])
+                for i, _ in enumerate(upcoming):
+                    value = '\u2620' + upcoming[i]['title']
+                    embed.add_field(name='', value=value, inline=True)
+
+        await ctx.send(content=None, embed=embed)
 
     def _calculate_timestamp(self, option):
 
@@ -37,18 +41,18 @@ class WebCrawler:
         # TODO fix hours so that upcoming will display correct events
 
         # 365 year and 30 days in a month
-        seconds_in_a_week = 6004800
+        seconds_in_a_week = 604800
         seconds_in_a_month = 2592000
         seconds_in_a_year = 31557600
 
         start = round(time.time())
 
         if option == 'month':
-            return start, start + seconds_in_a_month
+            return start, (start + seconds_in_a_month)
         elif option == 'week':
-            return start, start + seconds_in_a_week
+            return start, (start + seconds_in_a_week)
         elif option == 'year':
-            return start, start + seconds_in_a_year
+            return start, (start + seconds_in_a_year)
 
 
 def setup(bot):
