2077b512807a75a7edc143804c738d095e9fa687
krekel <luisg.diazpr@gmail.com>
2018-06-19 13:03:42 -0400

    *Implemented Async with hexdump. *Bytes will be read by chunks instead of saving it all in memory.

--- cogs/tools.py
+++ cogs/tools.py
@@ -17,58 +17,31 @@ class PwnTools:
 
     @tools.command()
     async def hexd(self, ctx):
+        author = ctx.message.author
         file = ctx.message.attachments[0]
         output = ''
         offset = 0
 
+        print(file.url)
+
         async with self.bot.session.get(file.url) as resp:
             while True:
-                chunk = await resp.read(16)
+                chunk = await resp.content.read(16)
+                print(chunk)
                 if len(chunk) == 0:
                     break
 
                 # offset
                 output += f'{offset:#08x} '
                 # hex content
-                output += ' '.join(f'{ord(char):02x}' for char in chunk.decode('UTF-8')) + ' '
+                output += ' '.join(f'{ord(char):02x}' for char in chunk.decode('ISO-8859-1')) + ' '
                 # decoded text
-                output += ''.join(f'{char}' if 32 < ord(char) < 128 else '.' for char in chunk.decode('UTF-8'))
+                output += ''.join(f'{char}' if 32 < ord(char) < 128 else '.' for char in chunk.decode('ISO-8859-1'))
                 output += '\n'
                 offset += 16
+        print(output)
 
-        # # Hex View
-        # hexview = []
-        # for x in range(2, len(data) + 2, 2):
-        #     hexview.append(data[x - 2:x])
-        #
-        # if len(hexview) % 16 != 0:
-        #     lines = (len(hexview) // 16) + 1
-        # else:
-        #     lines = len(hexview) // 16
-        #
-        # hexview.reverse()
-        #
-        # offset = 0x0
-        # file_dump = ''
-        # decoded_text = ''
-        #
-        # for _ in range(lines):
-        #     # add the offset
-        #     file_dump += str(hex(offset).replace('0x', '')).zfill(8) + ': '
-        #     # add hex content with decoded text
-        #     for _ in range(16):
-        #         if len(hexview) != 0:
-        #             byte = hexview.pop()
-        #             decoded_text += byte
-        #             file_dump += byte + ' '
-        #             offset += 1
-        #         else:
-        #             break
-        #     file_dump += bytearray.fromhex(decoded_text).decode() + '\n'
-        #     decoded_text = ''
-        # print(file_dump)
-
-        await ctx.send(file_dump)
+        await ctx.send(output)
 
     @tools.command()
     async def b64e(self, ctx, plaintext):
